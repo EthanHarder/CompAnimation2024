29aba3fb83fa11d0ca62760884374a8ef2f9aab4
Ethan <ethan.harder@ontariotechu.net>
2024-10-07 22:37:43 -0400

    PushLab2

---
+++ LAB02/Lab2/Assets/Scripts/PathController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    public Animator animator;
+    private bool isWalking;
+    
+    [SerializeField]
+    public PathManager pathManager;
+
+    List<Waypoint> thePath;
+    Waypoint target;
+
+    public float MoveSpeed;
+    public float RotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isWalking = false;
+        animator.SetBool("isWalking", isWalking);
+        
+        thePath = pathManager.GetPath();
+        Debug.Log(thePath[0].pos.x+", "+thePath[0].pos.z);
+        if (thePath != null && thePath.Count > 0)
+        {
+            target = thePath[0];
+        }
+    }
+
+    void rotateTowardTarget()
+    {
+        float stepSize = RotateSpeed * Time.deltaTime;
+
+        Vector3 targetDir = target.pos - transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, stepSize, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+
+    void moveForward()
+    {
+        float stepSize = Time.deltaTime * MoveSpeed;
+        float distanceToTarget = Vector3.Distance(transform.position, target.pos);
+        if (distanceToTarget < stepSize)
+        {
+            return;
+        }
+
+        Vector3 moveDir = Vector3.forward;
+        transform.Translate(moveDir * stepSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            isWalking = !isWalking;
+            animator.SetBool("isWalking", isWalking);
+        }
+
+        if (isWalking)
+        {
+            rotateTowardTarget();
+            moveForward();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        target = pathManager.GetNextTarget();
+        Debug.Log("next");
+        Debug.Log(thePath[0].pos.x + ", " + thePath[0].pos.z);
+    }
+}
